59b8ed80b3be60fcf5adbf041c691bbf14d54e78
Caboose19 <48289097+Caboose19@users.noreply.github.com>
2020-11-20 14:43:02 -0700

    Fixed issue now rotates and displays texutre plus cube added camera as well so can see more parts of cube at once

--- Application/Main.cpp
+++ Application/Main.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "Engine/Graphics/Renderer.h"
 #include "Engine\Graphics\Program.h"
+#include "Engine/Graphics/Texture.h"
 
 int main(int argc, char** argv)
 {
@@ -8,18 +9,78 @@ int main(int argc, char** argv)
 	renderer.StartUp();
 	renderer.Create("OpenGL", 800, 600);
 
-	float vertices[] =
-	{
-		-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
-		0.0f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 
-		0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
+	static float vertices[] = {
+		// front
+		-1.0f, -1.0f,  1.0f, 0.0f, 0.0f,
+		 1.0f, -1.0f,  1.0f, 1.0f, 0.0f,
+		 1.0f,  1.0f,  1.0f, 1.0f, 1.0f,
+		-1.0f,  1.0f,  1.0f, 0.0f, 1.0f,
+		// back
+		-1.0f, -1.0f, -1.0f, 1.0f, 0.0f,
+		 1.0f, -1.0f, -1.0f, 0.0f, 0.0f,
+		 1.0f,  1.0f, -1.0f, 0.0f, 1.0f,
+		-1.0f,  1.0f, -1.0f, 1.0f, 1.0f,
 	};
+
+	static GLushort indices[] =
+	{
+		// front
+		0, 1, 2,
+		2, 3, 0,
+		// right
+		1, 5, 6,
+		6, 2, 1,
+		// back
+		7, 6, 5,
+		5, 4, 7,
+		// left
+		4, 0, 3,
+		3, 7, 4,
+		// bottom
+		4, 5, 1,
+		1, 0, 4,
+		// top
+		3, 2, 6,
+		6, 7, 3
+	};
+
 
 	nc::Program program;
 	program.CreateShaderFromFile("shaders\\basic.vert", GL_VERTEX_SHADER);
 	program.CreateShaderFromFile("shaders\\basic.frag", GL_FRAGMENT_SHADER);
 	program.Link();
 	program.Use();
+
+	//create vertex buffer
+	GLuint vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER ,sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	//set position pipeline
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), 0);
+	glEnableVertexAttribArray(0);
+
+	//set uv pipline
+	glVertexAttribPointer(1,2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	//create index buffer
+	GLuint ibo;
+	glGenBuffers(1, &ibo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,ibo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	//uniform
+	glm::mat4 transform = glm::mat4(1.0f);
+	program.SetUniform("transform", transform);
+
+	glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800 / 600.0f, 0.01f,1000.0f);
+	glm::mat4 camera = glm::lookAt(glm::vec3(0, 02, -5), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+
+	nc::Texture texture;
+	texture.CreateTexture("texture\\llama.jpg");
+
 
 	bool quit = false;
 	while (!quit)
@@ -41,10 +102,15 @@ int main(int argc, char** argv)
 
 		SDL_PumpEvents();
 		
+		transform = glm::rotate(transform, 0.0004f, glm::vec3(0, 1, 0));
+		glm::mat4 mvp = projection * camera * transform;
+		program.SetUniform("transform", mvp);
+
 		renderer.BeginFrame();
 
 		//render triangle
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		GLsizei numElements = sizeof(indices) / sizeof(GLushort);
+		glDrawElements(GL_TRIANGLES, numElements, GL_UNSIGNED_SHORT, 0);
 
 		renderer.EndFrame();
 
